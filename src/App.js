83efdf91e3ca3fca87ddefc67e2e71e2062bd92a
Gunnar Carlson <gunnar.carlson@gmail.com>
2019-05-12 14:58:04 -0500

    Added mission-critical emojis

--- src/App.js
+++ src/App.js
@@ -23,7 +23,7 @@ class App extends Component {
   	  	  	<div className="App">
   	  	  	  	<header className="App-header">
   	  	  	  		<h1>Cadence</h1>
-									<h4>Workout playlist generator</h4>
+									<h4>🚴 Workout playlist generator 🏃</h4>
   	  	  	  	</header>
   	  	  	  	<Body 
   	  	  	  		token={this.props.accessToken} 
