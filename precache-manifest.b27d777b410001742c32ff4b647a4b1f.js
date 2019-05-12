ec37a3b9f7e890df8a9364104311008581a5ccc0
Gunnar Carlson <gunnar.carlson@gmail.com>
2019-05-12 13:26:20 -0500

    Updates

---
+++ precache-manifest.b27d777b410001742c32ff4b647a4b1f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bc9045ee0fa919c7ec72e8d45d38e9b7",
+    "url": "/cadence/index.html"
+  },
+  {
+    "revision": "bd01d236d3aea1cb720d",
+    "url": "/cadence/static/css/2.6d7d54d2.chunk.css"
+  },
+  {
+    "revision": "cd4854859abb496474ad",
+    "url": "/cadence/static/css/main.cd51d1a2.chunk.css"
+  },
+  {
+    "revision": "bd01d236d3aea1cb720d",
+    "url": "/cadence/static/js/2.8d05216f.chunk.js"
+  },
+  {
+    "revision": "cd4854859abb496474ad",
+    "url": "/cadence/static/js/main.a953fda7.chunk.js"
+  },
+  {
+    "revision": "a86de36a957aa709c92b",
+    "url": "/cadence/static/js/runtime~main.e9a39147.js"
+  }
+]);
